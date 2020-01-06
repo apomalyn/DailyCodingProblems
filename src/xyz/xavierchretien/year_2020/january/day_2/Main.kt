98f49b95a216f7d26d62b294df2c51731a0eff19
apomalyn <chretienxavier42@gmail.com>
2020-01-06 09:45:58 -0500

    Documenting day 2

--- src/xyz/xavierchretien/year_2020/january/day_2/Main.kt
+++ src/xyz/xavierchretien/year_2020/january/day_2/Main.kt
@@ -1,12 +1,20 @@
 package xyz.xavierchretien.year_2020.january.day_2
 
+import java.lang.IllegalArgumentException
+
 fun main() {
     println(findFixedPoint(intArrayOf(-6, 0, 2, 4, 5)))
     println(findFixedPoint(intArrayOf(1, 5, 7, 8)))
 }
 
-fun findFixedPoint(array: IntArray): MutableList<Int>?{
-    if(array[1] < array[0]) return null
+/**
+ * Find each fixed points in a sorted array.
+ *
+ * @param array, sorted array
+ * @return an list with each fixed points, the list is empty if there is no fixed points.
+ */
+fun findFixedPoint(array: IntArray): List<Int>{
+    if(array[1] < array[0]) throw IllegalArgumentException("Array not sorted")
 
     val pointFixedFound = mutableListOf<Int>()
     for ((index, value) in array.withIndex()) {
@@ -15,5 +23,5 @@ fun findFixedPoint(array: IntArray): MutableList<Int>?{
         }
     }
 
-    return if(pointFixedFound.size > 0) pointFixedFound else null
+    return pointFixedFound.toList()
 }
